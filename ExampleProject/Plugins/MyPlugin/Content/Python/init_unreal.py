f1f9168b1a58b2054ab99262f4e521a357bf498a
Rajat Goswami <rajat@imgspc.com>
2020-09-25 12:01:38 -0400

    Adding Japanese translation

--- ExampleProject/Plugins/MyPlugin/Content/Python/init_unreal.py
+++ ExampleProject/Plugins/MyPlugin/Content/Python/init_unreal.py
@@ -10,20 +10,15 @@ imgspc.make_menu_item(menu_path='ImgSpc/Import...', callback='importer.import_fi
 imgspc.make_menu_item(menu_path='ImgSpc/Export...', callback='exporter.export_file(".gltf")', tooltip='Export file')
 
 # Create Help sub-menu and add About menu-item
-imgspc.make_menu_item(
-    menu_path='ImgSpc/Help/About Imaginary Spaces', 
-    callback='utilities.load_docs()', 
-    icon_path='MyPlugin/Resources/imgspc-logo-large.png', 
-    tooltip='About Imaginary Spaces'
-)
+imgspc.make_menu_item(menu_path='ImgSpc/Help/About Imaginary Spaces', callback='utilities.load_docs()', tooltip='About Imaginary Spaces')
 
-# Add to the Main Menu bar
+# Add to the Main Menu bar (in Japanese)
 imgspc.make_menu_item('LevelEditor.MainMenu/ImgSpc')
-imgspc.make_menu_item('LevelEditor.MainMenu/ImgSpc/Import...', callback='importer.import_file()', tooltip='Import file')
-imgspc.make_menu_item('LevelEditor.MainMenu/ImgSpc/Export...', callback='exporter.export_file(".gltf")', tooltip='Export file')
+imgspc.make_menu_item('LevelEditor.MainMenu/ImgSpc/インポート...', callback='importer.import_file()', tooltip='インポート')
+imgspc.make_menu_item('LevelEditor.MainMenu/ImgSpc/エクスポート...', callback='exporter.export_file(".gltf")', tooltip='エクスポート')
 imgspc.make_menu_item(
-    'LevelEditor.MainMenu/ImgSpc/Help/About Imaginary Spaces',
+    'LevelEditor.MainMenu/ImgSpc/ヘルプ/Imaginary Spaces について',
     callback='utilities.load_docs()',
     icon_path='MyPlugin/Resources/imgspc-logo-large.png',
-    tooltip='About Imaginary Spaces'
+    tooltip='Imaginary Spaces について'
 )
